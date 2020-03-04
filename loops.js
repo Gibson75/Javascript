4dc7888b6bf7bfea3b3c3558696ec39c45f49cf2
Gibson75 <60351517+Gibson75@users.noreply.github.com>
2020-03-04 10:19:56 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,48 @@
+var moviesArray = ["Casablanca", "Star Wars", "Singing in the Rain", "The Wizard of Oz", "Die Hard"];
+
+var i = 0; 
+
+while(i < moviesArray.length){
+  console.log(moviesArray[i]);
+  i++;
+}
+
+
+
+for(var i = 0; i < moviesArray.length; i++){
+  console.log(moviesArray[i]);
+}
+
+
+for(var i = 0; i < moviesArray.length; i++){
+  console.log("Tweet Tweet Tweet!");
+}
+
+
+var favoriteFoodsArray = ["Tacos","Pizza", "Ribs", "Mochi","Grilled Cheese"]
+
+for(var i = 0; i < favoriteFoodsArray.length; i++){
+  console.log(favoriteFoodsArray[i]);
+}
+
+
+
+var interestsArray = ["Painting", "Reading", "Gaming", "Sporting", "Sky Watching"]
+
+for(var i = 0; i < interestsArray.length; i++){
+
+  console.log(`One of my interests is: ${interestsArray[i]}`);
+}
+
+
+function sayHelloWorld(){
+  console.log("Hello, world!");
+  console.log("Hola, Tacos por favor")
+}
+
+
+sayHelloWorld(); 
+
+sayHelloWorld(); 
+
+
